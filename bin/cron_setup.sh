21718c784192e87da5f607b786478fba282d3d8d
epfremme <epfremme@nerdery.com>
2015-07-25 22:13:40 -0500

    Fixed script file path bug

--- bin/cron_setup.sh
+++ bin/cron_setup.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
-FILENAME=${SCRIPTPATH}/files/cron.txt
+FILENAME=${SCRIPTPATH}/../files/cron.txt
+popd > /dev/null
 
 sudo crontab -l | cat - ${FILENAME} | sudo crontab -
