6dfa077525e72cec641510a2d024d7dc5db17fd9
Ben Koren <benkoren@gmail.com>
2015-12-20 21:13:44 +0000

    Minor fixes to install process.

--- bin/install_minecraft.sh
+++ bin/install_minecraft.sh
@@ -14,15 +14,16 @@ cd /etc/minecraft
 sudo wget -O minecraft_server.jar https://s3.amazonaws.com/Minecraft.Download/versions/1.8.7/minecraft_server.1.8.7.jar
 
 # Install minecraft overviewer
+echo "deb http://overviewer.org/debian ./" | sudo tee -a /etc/apt/sources.list
 wget -O - http://overviewer.org/debian/overviewer.gpg.asc | sudo apt-key add -
 sudo apt-get update
-apt-get install minecraft-overviewer
+sudo apt-get install minecraft-overviewer
 
 # return to bin dir
 cd ${SCRIPTPATH}
 
 # Install minecraft init.d script
-cp ./minecraft_service.sh /etc/init.d/
+sudo cp ./minecraft_service.sh /etc/init.d/minecraft
 
 # Setup backup/map crons
 ./cron_setup.sh
